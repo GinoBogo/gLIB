85369da0f8c44a12751ee353503349cf829173af
Gino Bogo <gino.bogo@gmail.com>
2021-03-15 11:16:15 +0100

    Added a test/example for "GMeasure" struct

---
+++ test/test_GMeasure.cpp
@@ -0,0 +1,32 @@
+#include "GMeasure.hpp"
+
+#include <fmt/core.h>
+#include <functional>
+
+int factorial(int n) {
+    if (n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+
+void factorial_loop(int loop, int number) {
+    auto result = 0;
+
+    while (loop-- > 0) {
+        result = factorial(number);
+    }
+
+    fmt::print(" - Factorial({}) = {}\n", number, result);
+}
+
+int main() {
+    std::function<void(int, int)> func = factorial_loop;
+
+    auto loop    = 20000;
+    auto elapsed = GMeasure::ns(func, loop, 23);
+
+    fmt::print(" - Elapsed time = {} us\n", elapsed / (double)loop);
+
+    return 0;
+}
