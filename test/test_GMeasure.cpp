79eef444445a32cbf9d10e69cac66dea4d78c16a
Gino Bogo <gino.bogo@gmail.com>
2021-03-17 08:16:48 +0100

    Factorial test: moved from recursive to real math

--- test/test_GMeasure.cpp
+++ test/test_GMeasure.cpp
@@ -4,31 +4,59 @@
 #include <fmt/core.h>
 #include <functional>
 
-int factorial(int n) {
-    if (n > 1)
-        return n * factorial(n - 1);
-    else
-        return 1;
-}
+int multiply(int x, int res[], int res_size) {
+    int carry = 0;
 
-void factorial_loop(int loop, int number) {
-    auto result = 0;
-
-    while (loop-- > 0) {
-        result = factorial(number);
+    for (int i = 0; i < res_size; i++) {
+        int prod = res[i] * x + carry;
+        res[i]   = prod % 10;
+        carry    = prod / 10;
     }
 
-    LOG_WRITE(debug, fmt::format(" - Factorial({}) = {}", number, result));
+    while (carry) {
+        res[res_size] = carry % 10;
+        carry         = carry / 10;
+        res_size++;
+    }
+    return res_size;
+}
+
+std::string factorial(int n) {
+    std::string value = "";
+
+    int res[2000];
+
+    res[0]       = 1;
+    int res_size = 1;
+
+    for (auto x = 2; x <= n; x++) {
+        res_size = multiply(x, res, res_size);
+    }
+
+    for (int i = res_size - 1; i >= 0; i--) {
+        value += std::to_string(res[i]);
+    }
+
+    return value;
+}
+
+void function_loop(int loop, int number, std::string *result) {
+    while (loop-- > 0) {
+        *result = factorial(number);
+    }
 }
 
 int main() {
     LOG_WRITE(trace, fmt::format("\"{}\" process STARTED", __func__));
 
-    std::function<void(int, int)> func = factorial_loop;
+    std::function<void(int, int, std::string *)> func = function_loop;
 
-    auto loop    = 20000;
-    auto elapsed = GMeasure::ns(func, loop, 23);
+    auto loop    = 80000;
+    auto number  = 69;
+    auto result  = std::string("");
+    auto elapsed = GMeasure::ns(func, loop, number, &result);
 
+    LOG_WRITE(debug, fmt::format(" - Factorial({}) = {}", number, result));
     LOG_WRITE(debug, fmt::format(" - Elapsed time = {} ns", elapsed / (double)loop));
 
     LOG_WRITE(trace, fmt::format("\"{}\" process STOPPED", __func__));
