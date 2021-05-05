d9a12d2337d11ba7b960150a20c54a6b2b5de9de
Gino Bogo <gino.bogo@gmail.com>
2021-05-05 09:00:59 +0200

    "union" trick to suppress "cast-align" warning

--- src/GFifo.cpp
+++ src/GFifo.cpp
@@ -11,6 +11,11 @@
 #include <cstdlib> // malloc, free
 #include <cstring> // memcpy, memset
 
+typedef union {
+    size_t *size;
+    char *  ptr;
+} size_ptr;
+
 GFifo::GFifo(size_t bytes, size_t depth) //
 : m_bytes(bytes + sizeof(size_t)),       //
   m_depth(depth) {
@@ -46,21 +51,21 @@ void GFifo::Zeros() {
 bool GFifo::Push(void *src_buffer, size_t src_bytes) {
     auto result{false};
     if (src_buffer != nullptr && src_bytes <= (m_bytes - sizeof(size_t))) {
-        char *__dst;
+        size_ptr __dst;
         {
             const std::lock_guard<std::mutex> lock(m_mutex);
             if (m_free > 0) {
                 --m_free;
                 ++m_used;
 
-                __dst = m_data + (m_iW * m_bytes);
+                __dst.ptr = m_data + (m_iW * m_bytes);
 
                 if (++m_iW == m_depth) {
                     m_iW = 0;
                 }
 
-                *((size_t *)__dst) = src_bytes;
-                memcpy(__dst + sizeof(size_t), src_buffer, src_bytes);
+                *__dst.size = src_bytes;
+                memcpy(__dst.ptr + sizeof(size_t), src_buffer, src_bytes);
                 result = true;
             }
             else {
@@ -74,21 +79,21 @@ bool GFifo::Push(void *src_buffer, size_t src_bytes) {
 bool GFifo::Pop(void *dst_buffer, size_t *dst_bytes) {
     auto result{false};
     if (dst_buffer != nullptr && dst_bytes != nullptr) {
-        char *__src;
+        size_ptr __src;
         {
             const std::lock_guard<std::mutex> lock(m_mutex);
             if (m_used > 0) {
                 ++m_free;
                 --m_used;
 
-                __src = m_data + (m_iR * m_bytes);
+                __src.ptr = m_data + (m_iR * m_bytes);
 
                 if (++m_iR == m_depth) {
                     m_iR = 0;
                 }
 
-                *dst_bytes = *((size_t *)__src);
-                memcpy(dst_buffer, __src + sizeof(size_t), *dst_bytes);
+                *dst_bytes = *__src.size;
+                memcpy(dst_buffer, __src.ptr + sizeof(size_t), *dst_bytes);
                 result = true;
             }
             else {
