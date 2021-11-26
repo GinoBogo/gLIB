2aee73498527555e803b1a0ce6fa5106e07685b3
Gino Bogo <gino.bogo@gmail.com>
2021-11-26 10:20:52 +0100

    Added the "LOG_MSG_MAXLEN" define. This define impacts the "GLogger::Format" function ony.

--- src/GLogger.hpp
+++ src/GLogger.hpp
@@ -12,10 +12,9 @@
 #include <cstdio> // snprintf
 #include <string> // string
 
-#define LOG_TYPE(type) GLogger::type, __FILE__, __LINE__
-
-#define LOG_WRITE(type, message) GLogger::Write(LOG_TYPE(type), message)
-
+#define LOG_MSG_MAXLEN                512
+#define LOG_TYPE(type)                GLogger::type, __FILE__, __LINE__
+#define LOG_WRITE(type, message)      GLogger::Write(LOG_TYPE(type), message)
 #define LOG_FORMAT(type, format, ...) GLogger::Format(LOG_TYPE(type), format, __VA_ARGS__)
 
 namespace GLogger {
@@ -27,7 +26,7 @@ namespace GLogger {
     void Write(Type type, const char *file, size_t line, const std::string &message);
 
     template <class... Args> void Format(Type type, const char *file, size_t line, const char *format, Args... args) {
-        char msg[512];
+        char msg[LOG_MSG_MAXLEN];
         snprintf(msg, sizeof msg, format, args...);
         Write(type, file, line, msg);
     }
